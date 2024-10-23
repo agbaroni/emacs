9bac929f5600ad4a9d27eec2cb0ba76a9c1b2f75
Alessio G. Baroni <alessiogiovanni.baroni@gmail.com>
2024-10-23 15:38:05 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,53 @@
+;; -*- lexical-binding: t; coding: utf-8; -*-
+
+(setq custom-file "custom.el")
+
+(setq gc-cons-percentage 0.25)
+
+(setq package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://stable.melpa.org/packages/")))
+
+(setq package-archive-priorities '(("melpa" . 1)
+				   ("elpa" . 2)))
+
+(package-refresh-contents)
+
+(package-initialize)
+
+(if (< emacs-major-version 29)
+    (package-install 'use-package))
+
+(use-package company
+  :config (global-company-mode)
+  :custom (company-backends '(company-capf
+			      company-dabbrev
+			      company-files))
+  :ensure t)
+
+(use-package consult
+  :ensure t)
+
+(use-package emacs
+  :config (progn
+	    (menu-bar-mode -1)
+	    (tool-bar-mode -1)
+	    (toggle-frame-fullscreen))
+  :ensure t)
+
+(use-package marginalia
+  :config (marginalia-mode)
+  :ensure t)
+
+(use-package rainbow-mode
+  :ensure t)
+
+(use-package undo-tree
+  :config
+  (global-undo-tree-mode)
+  :ensure t)
+
+(use-package vertico
+  :config (vertico-mode)
+  :ensure t)
+
+(load custom-file t)
